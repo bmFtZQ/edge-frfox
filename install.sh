5c280b875a937108caca5631b0fd04d62e28fa8f
DanielSGH <daniel.st2k@gmail.com>
2023-12-24 23:56:42 +0000

    unpack repo tarball, add optional tweaks support, fix macOS issues, remove unneeded code, modify user.js instead

--- install.sh
+++ install.sh
@@ -6,29 +6,33 @@ PROJECT_NAME=$(basename $GITHUB_REPO | cut -d '.' -f 1)
 TMP_DIR="${TMPDIR:-$(dirname $(mktemp))}"
 
 if [[ $OSTYPE == "darwin"* ]]; then
-  FIREFOX_DIR=$HOME/Library/Application\ Support/Firefox/Profiles
+  FIREFOX_DIR=$HOME/Library/Application\ Support/Firefox
 else
   FIREFOX_DIR=$HOME/.mozilla/firefox
 fi;
 
-PROFILE_ROOTDIR=$FIREFOX_DIR/$(grep -E "Path=.*\.(dev-edition-default|default-.*)" $FIREFOX_DIR/profiles.ini | tail -1 | cut -c 6-);
-CHANGED_PREFS=("toolkit.legacyUserProfileCustomizations.stylesheets" "svg.context-properties.content.enabled" "layout.css.color-mix.enabled")
+PROFILE_ROOTDIR=$FIREFOX_DIR/$(grep -E "Path=.*\.(dev-edition-default|default-.*)" "$FIREFOX_DIR/profiles.ini" | tail -1 | cut -c 6-);
+OPTIONALS=(
+  "widget.macos.native-context-menus" "true"
+  "browser.theme.dark-private-windows" "true"
+)
 
 # COLORS
 GREEN='\033[0;32m'
 YELLOW='\033[0;93m'
 NC='\033[0m'
 CYAN='\033[0;36m'
+RED='\033[0;31m'
 
 # UTILITY FUNCTIONS
 set_pref() {
-  echo "setting $1 to $2 in prefs.js";
-  echo "user_pref(\"$1\", $2);" >> $PROFILE_ROOTDIR/prefs.js;
+  echo "setting $1 to $2 in user.js";
+  echo "user_pref(\"$1\", $2);" >> $PROFILE_ROOTDIR/user.js;
 }
 
 delete_pref() {
   echo "resetting $1 to default"
-  sed -i "/user_pref(\"$1\", \(true\|false\));/d" $PROFILE_ROOTDIR/prefs.js;
+  sed -i "/user_pref(\"$1\", \(true\|false\));/d" $PROFILE_ROOTDIR/user.js;
 }
 
 #####################
@@ -38,13 +42,17 @@ delete_pref() {
 firefox_proc=$(pgrep firefox);
 if [ ! -z $firefox_proc ]; then
   echo "Before (un)installing, please make sure firefox is not running."
-  echo "Otherwise, changes cannot be made to prefs.js"
+  echo "Otherwise, changes cannot be made to user.js"
   exit 0;
 fi
 
 # Prompting for correct install directory
-read -e -i "$PROFILE_ROOTDIR" -p $'Enter profile root directory: \e[36m' newdir
-echo -e -n "${NC}"
+echo "Please enter the path to your firefox profile directory."
+echo "This can be found by opening about:support in firefox and looking for the Profile root directory."
+echo "Press CTRL+C to abort installation now."
+
+echo -e "${CYAN}Automatically detected: $PROFILE_ROOTDIR${NC}"
+read -e -p "Press ENTER to use this directory, or type in a new one: " newdir
 PROFILE_ROOTDIR="${newdir:-$PROFILE_ROOTDIR}"
 
 if [ ! -d "$PROFILE_ROOTDIR" ]; then
@@ -63,7 +71,7 @@ fi
 if [[ $1 == "uninstall" ]]; then  
   echo -e "${YELLOW}NOTE: This is the final opportunity to abort uninstallation by pressing Ctrl+C${NC}";
   ans="n"
-  read -e -i "$ans" -p "Do you want to delete $PROFILE_ROOTDIR/chrome? (y/n): " in
+  read -e -p "Do you want to delete $PROFILE_ROOTDIR/chrome? [y/N]: " in
   ans="${in:-$ans}";
 
   if [[ ${ans,,} =~ ^(yes|y)$ ]]; then
@@ -71,31 +79,11 @@ if [[ $1 == "uninstall" ]]; then
   fi
 
   echo "uninstalling...";
-  for pref in ${CHANGED_PREFS[@]}; do
-    delete_pref $pref;
-  done;
+
+  rm -rf $PROFILE_ROOTDIR/user.js
 
   echo "uninstall complete."
   exit 0;
-fi
-
-git --version 2>&1 > /dev/null;
-if [ ! $? -eq 0 ]; then
-  echo "ERROR: git is not installed... Please install it."
-  exit 0;
-fi
-
-echo "Detecting if firefox is installed on your system..."
-if [ ! -f /usr/bin/firefox ] && { [ ! -f /usr/lib/firefox/firefox ] && [ ! -f /usr/lib/firefox-developer-edition/firefox ]; }; then
-  echo "ERROR: firefox not found..."
-
-  ans="y"
-  read -e -i "$ans" -p "Do you want to continue anyway? (y/n): " in
-  ans="${in:-$ans}";
-
-  if [[ ! ${ans,,} =~ ^(yes|y)$ ]]; then
-    exit 0;
-  fi
 fi
 
 #################
@@ -103,25 +91,32 @@ fi
 #################
 
 echo "Installing..."
-if [ ! -d $TMP_DIR/$PROJECT_NAME ]; then
-  echo "cloning repository";
-  if ! git clone $GITHUB_REPO $TMP_DIR/$PROJECT_NAME; then
-    echo "Error while cloning repository into $TMP_DIR/$PROJECT_NAME..."
-    exit 0;
-  fi
+if ! curl -L "https://github.com/bmFtZQ/edge-frfox/archive/refs/heads/main.tar.gz" | tar xz -C $TMP_DIR --strip-components=1 --one-top-level=$PROJECT_NAME; then
+  echo -e "${RED}Installation failed: ${NC}Failed to get github repo tarball and extract it";
+  exit 0;
 fi
 
-echo "Copying theme folder..."
-cp -r $TMP_DIR/$PROJECT_NAME/chrome "$PROFILE_ROOTDIR"
+echo "Copying theme folder...";
+cp -r $TMP_DIR/$PROJECT_NAME/chrome $PROFILE_ROOTDIR;
+cp $TMP_DIR/$PROJECT_NAME/user.js $PROFILE_ROOTDIR;
 
-# firefox will automatically sort out any duplicate issues, whatever is at the end of the file takes priority, so this works.
-echo "Adding necessary configs..."
-for pref in ${CHANGED_PREFS[@]}; do
-  set_pref $pref "true"
+#####################
+# OPTIONAL SETTINGS #
+#####################
+
+echo "Optional settings, refer to https://github.com/bmFtZQ/edge-frfox/tree/main#how-to-install";
+for ((i = 0; i < ${#OPTIONALS[@]}; i += 2)); do
+  ans="n"
+  read -e -p "Set ${OPTIONALS[i]} to ${OPTIONALS[i+1]?} [y/N]: " in
+  ans="${in:-$ans}";
+
+  if [[ ! ${ans,,} =~ ^(yes|y)$ ]]; then
+    continue;
+  fi
+
+  set_pref ${OPTIONALS[i]} ${OPTIONALS[i+1]}
+
 done;
 
-if [[ $OSTYPE == "darwin"* ]]; then
-  set_pref "widget.macos.native-context-menus" "false"
-fi
 
 echo -e "${GREEN}Finished successfully! Please start firefox to see the changes.";
