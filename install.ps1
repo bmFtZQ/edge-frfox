2df1f9a61b5dd7b17a68d99ad671a9b15a955804
DanielSGH <daniel.st2k@gmail.com>
2023-12-26 20:04:00 +0000

    Windows: made equivalent to linux bash script + uninstall removes user.js lines, not file

--- install.ps1
+++ install.ps1
@@ -5,7 +5,10 @@ $GITHUB_REPO="https://github.com/bmFtZQ/edge-frfox.git"
 $PROJECT_NAME=$($GITHUB_REPO.Split("/")[-1] -replace ".git", "")
 $FIREFOX_DIR="$env:APPDATA\Mozilla\Firefox";
 $PROFILE_ROOTDIR="$($FIREFOX_DIR)\Profiles\$((Select-String -Path "$($FIREFOX_DIR)\profiles.ini" -Pattern "Path=.*\.(dev-edition-default|default-.*)" | Select-Object -Last 1).Line.Substring(14))";
-$CHANGED_PREFS=@("toolkit.legacyUserProfileCustomizations.stylesheets", "svg.context-properties.content.enabled", "layout.css.color-mix.enabled");
+$OPTIONALS=@{
+  "widget.macos.native-context-menus" = "false"
+  "browser.theme.dark-private-windows" = "false"
+};
 
 # UTILITY FUNCTIONS
 function set_pref {
@@ -14,8 +17,8 @@ function set_pref {
     $Bool
   )
 
-  Write-Output "setting $pref to $bool in prefs.js";
-	"user_pref(`"$pref`", $bool);" | out-file "$PROFILE_ROOTDIR/prefs.js" -Encoding ASCII -append
+  Write-Output "setting $pref to $bool in user.js";
+	"user_pref(`"$pref`", $bool);" | out-file "$PROFILE_ROOTDIR\user.js" -append
 }
 
 function delete_pref {
@@ -23,12 +26,12 @@ function delete_pref {
     $Pref
   )
 
-  $Prefsjs="$PROFILE_ROOTDIR\prefs.js";
+  $Userjs="$PROFILE_ROOTDIR\user.js";
 
   Write-Output "resetting $Pref to default";
-  (Get-Content -Path $Prefsjs) |
-    ForEach-Object {$_ -Replace "user_pref\(\`"$Pref\`", (true|false)\);", ''} |
-      Set-Content -Path $Prefsjs;
+  (Get-Content "$PROFILE_ROOTDIR\user.js")  | 
+  Where-Object {$_ -ne $Pref}               | 
+  Set-Content -Path $Userjs;
 }
 
 #####################
@@ -37,7 +40,7 @@ function delete_pref {
 
 $firefox_proc=Get-Process firefox -ErrorAction SilentlyContinue;
 if ($firefox_proc) {
-  Write-Host "ERROR: Before installing, please make sure firefox is not running.`nOtherwise, changes cannot be made to prefs.js" -ForegroundColor Red;
+  Write-Host "ERROR: Before installing, please make sure firefox is not running.`nOtherwise, changes cannot be made to user.js" -ForegroundColor Red;
   exit 0;
 }
 
@@ -51,38 +54,6 @@ Write-Output "Press ENTER to use this directory, or type in a new one.";
 $ans=Read-Host "Path";
 $PROFILE_ROOTDIR=($PROFILE_ROOTDIR,$ans)[[bool]$ans];
 
-# Check if issued `...\installer.sh uninstall`
-if ($args[0] -eq "uninstall") {
-  Remove-Item "$PROFILE_ROOTDIR/chrome" -Recurse -Confirm:$true;
-   
-  Write-Output "uninstalling...";
-  foreach ($pref in $CHANGED_PREFS) {
-    delete_pref $pref;
-  }
-
-  Write-Output "uninstall complete.";
-  exit 0;
-}
-
-Write-Output "Checking if git is installed...";
-if (!(Get-Command git -ErrorAction SilentlyContinue)) {
-  Write-Host "ERROR: git not found... Please install git and try again." -ForegroundColor Red;
-  exit 0;
-}
-
-Write-Output "Detecting if firefox is installed on your system..."
-if (!(Test-Path $FIREFOX_DIR)) {
-  Write-Host "ERROR: firefox not found..." -ForegroundColor Red;
-  
-  Write-Output "Do you want to continue anyway?";
-  $continue=Read-Host -Prompt "y/n";
-
-	if (!("$continue".ToLower() -match "^(y|yes)$")) {
-		Write-Output "Aborting installation...";
-		exit 0;
-	}
-}
-
 if (!(Test-Path "$PROFILE_ROOTDIR")) {
 	Write-Host "ERROR: firefox profile directory could not be found..." -ForegroundColor Red;
 
@@ -94,25 +65,69 @@ if (!(Test-Path "$PROFILE_ROOTDIR")) {
 	} while (!(Test-Path "$PROFILE_ROOTDIR"));
 }
 
+# Check if issued `...\installer.sh uninstall`
+if ($args[0] -eq "uninstall") {
+  Write-Host "NOTE: This is the final opportunity to abort uninstallation by pressing Ctrl+C" -ForegroundColor Yellow;
+  Remove-Item "$PROFILE_ROOTDIR\chrome" -Recurse -Confirm:$true;
+
+  # Download changed user.js from online
+  $fail=$false
+  Invoke-Webrequest -Uri "https://raw.githubusercontent.com/bmFtZQ/edge-frfox/main/user.js" -UseBasicParsing -OutFile "$env:temp\user.js"
+  if (!($?)) {
+    Write-Host "WARNING: Failed to retrieve online user.js prefs. Continuing..." -ForegroundColor Yellow
+    $fail=$true
+  }
+
+  Write-Output "uninstalling...";
+  if (!$fail) {
+    $FETCHED_PREFS= @()
+    $FETCHED_PREFS += Select-String -Path "$env:temp\user.js" -Pattern "user_pref\(`"([a-zA-Z0-9.-]+)`",\s*(true|false)\);" | ForEach-Object {$_.Line -replace ".*\\", ""}
+    foreach ($pref in $FETCHED_PREFS) {
+      delete_pref "$pref";
+    }
+  } 
+
+  Write-Output "uninstall complete.";
+  exit 0;
+}
+
 #################
 # INSTALL PHASE #
 #################
 
 Write-Output "Installing...";
-if (!(Test-Path "$("$env:temp\$PROJECT_NAME")")) {
-	Write-Output "Cloning $GITHUB_REPO into $env:temp\$PROJECT_NAME...";
+Write-Output "Downloading source code...";
 
-	git clone $GITHUB_REPO "$env:temp\$PROJECT_NAME";
+$response = Invoke-WebRequest -Uri "https://github.com/bmFtZQ/edge-frfox/archive/refs/heads/main.zip" -OutFile "$env:temp\edge-frfox.zip"
+if (!($?)) {
+  Write-Host "ERROR: Failed to download source code from https://github.com/bmFtZQ/edge-frfox/archive/refs/heads/main.zip." -ForegroundColor Red;
+  Write-Host "$($response.StatusCode)"
+  exit 0;
 }
 
-Write-Output "Copying files to $PROFILE_ROOTDIR\chrome...";
-Remove-Item "$PROFILE_ROOTDIR\chrome" -Recurse -ErrorAction SilentlyContinue -Confirm:$false -Force;
-Copy-Item "$env:temp\$PROJECT_NAME\chrome" -Destination $PROFILE_ROOTDIR -Recurse -Force;
+Expand-Archive -Path "$env:temp\$PROJECT_NAME.zip" -DestinationPath "$env:temp" -Force
+if (!($?)) {
+  Write-Host "ERROR: Failed to extract $env:temp\$PROJECT_NAME.zip." -ForegroundColor Red;
+  Write-Host "Message: $($Error[0].Exception.Message)";
+  exit 0;
+}
+
+echo "Copying theme folder..."
+Copy-Item -Recurse -Path "$env:temp\$PROJECT_NAME-main\chrome" -Destination $PROFILE_ROOTDIR
+Get-Content "$env:temp\$PROJECT_NAME-main\user.js" | Tee-Object -Append "$PROFILE_ROOTDIR\user.js" | Out-Null
 
 # firefox will automatically sort out any duplicate issues, whatever is at the end of the file takes priority, so this works.
 Write-Output "Setting preferences...";
-foreach ($pref in $CHANGED_PREFS) {
-	set_pref $pref "true";
+foreach ($key in $OPTIONALS.Keys) {
+  $ans = "n"
+  $in = Read-Host -Prompt "Set $key to $($optionals[$key]) [y/N]"
+  $ans = if ($in) { $in } else { $ans }
+
+  if (!($ans.ToLower() -match "^(yes|y)$")) {
+    continue
+  }
+
+  set_pref "$key" "$($optionals[$key])";
 }
 
 Write-Host "Installation complete! Please start firefox to see the changes.`n" -ForegroundColor Green;
